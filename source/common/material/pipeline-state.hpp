e36034e321c828350a982d7fb6a64542ca16462c
Mohamed Elsemary <mohamedelsemary19@yahoo.com>
2025-11-03 02:23:58 +0200

    Req 4 is done

--- source/common/material/pipeline-state.hpp
+++ source/common/material/pipeline-state.hpp
@@ -42,6 +42,37 @@ namespace our {
         // For example, if faceCulling.enabled is true, you should call glEnable(GL_CULL_FACE), otherwise, you should call glDisable(GL_CULL_FACE)
         void setup() const {
             //TODO: (Req 4) Write this function
+            // Configure Face Culling
+            if (faceCulling.enabled) {
+                glEnable(GL_CULL_FACE);
+                glCullFace(faceCulling.culledFace);
+                glFrontFace(faceCulling.frontFace);
+            } else {
+                glDisable(GL_CULL_FACE);
+            }
+
+            // Configure Depth Testing
+            if (depthTesting.enabled) {
+                glEnable(GL_DEPTH_TEST);
+                glDepthFunc(depthTesting.function);
+            } else {
+                glDisable(GL_DEPTH_TEST);
+            }
+
+            // Configure Blending
+            if (blending.enabled) {
+                glEnable(GL_BLEND);
+                glBlendEquation(blending.equation);
+                glBlendFunc(blending.sourceFactor, blending.destinationFactor);
+                glBlendColor(blending.constantColor.r, blending.constantColor.g, blending.constantColor.b, blending.constantColor.a);
+            } else {
+                glDisable(GL_BLEND);
+            }
+
+            // Configure Color and Depth Masks
+            // Note: glm::bvec4 components can be accessed via .r, .g, .b, .a or .x, .y, .z, .w
+            glColorMask(colorMask.r, colorMask.g, colorMask.b, colorMask.a);
+            glDepthMask(depthMask);
         }
 
         // Given a json object, this function deserializes a PipelineState structure
