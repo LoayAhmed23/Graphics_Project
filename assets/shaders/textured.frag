1fbf254577d5a94f1f43be974d0e5bc20a45119d
Mohamed Elsemary <mohamedelsemary19@yahoo.com>
2025-11-03 03:52:05 +0200

    Req 7 is done

--- assets/shaders/textured.frag
+++ assets/shaders/textured.frag
@@ -9,9 +9,19 @@ out vec4 frag_color;
 
 uniform vec4 tint;
 uniform sampler2D tex;
-
+uniform float alphaThreshold; // Added for alpha testing
 void main(){
     //TODO: (Req 7) Modify the following line to compute the fragment color
     // by multiplying the tint with the vertex color and with the texture color 
-    frag_color = vec4(1.0);
+    // 1. Sample the texture
+    vec4 textureColor = texture(tex, fs_in.tex_coord);
+
+    // 2. Check for alpha threshold
+    if(textureColor.a < alphaThreshold) {
+        discard; // Discard this fragment if its alpha is below the threshold
+    }
+
+    // 3. Compute the final color
+    frag_color = textureColor * fs_in.color * tint;
+   // frag_color = vec4(1.0);
 }
--- source/common/material/material.cpp
+++ source/common/material/material.cpp
@@ -8,6 +8,10 @@ namespace our {
     // This function should setup the pipeline state and set the shader to be used
     void Material::setup() const {
         //TODO: (Req 7) Write this function
+        // 1. Setup the pipeline state
+        pipelineState.setup();
+        // 2. Use the shader program
+        shader->use();
     }
 
     // This function read the material data from a json object
@@ -25,6 +29,10 @@ namespace our {
     // set the "tint" uniform to the value in the member variable tint 
     void TintedMaterial::setup() const {
         //TODO: (Req 7) Write this function
+        // 1. Call the parent's setup
+        Material::setup();
+        // 2. Set the "tint" uniform
+        shader->set("tint", tint);
     }
 
     // This function read the material data from a json object
@@ -39,6 +47,34 @@ namespace our {
     // Then it should bind the texture and sampler to a texture unit and send the unit number to the uniform variable "tex" 
     void TexturedMaterial::setup() const {
         //TODO: (Req 7) Write this function
+        // 1. Call the parent's setup (which sets the tint)
+        TintedMaterial::setup();
+        
+        // 2. Set the "alphaThreshold" uniform
+        shader->set("alphaThreshold", alphaThreshold);
+
+        // 3. Set the texture and sampler
+        // We'll use texture unit 0
+        glActiveTexture(GL_TEXTURE0);
+
+        // If the texture is not null, bind it
+        if (texture) {
+            texture->bind();
+        } else {
+            // Otherwise, unbind the texture unit
+            Texture2D::unbind();
+        }
+
+        // If the sampler is not null, bind it
+        if (sampler) {
+            sampler->bind(0); // Bind to texture unit 0
+        } else {
+            // Otherwise, unbind the sampler from the unit
+            Sampler::unbind(0);
+        }
+
+        // 4. Send the texture unit index (0) to the "tex" uniform
+        shader->set("tex", 0);
     }
 
     // This function read the material data from a json object
